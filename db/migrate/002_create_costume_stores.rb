7509dd293e9a29d384e170c5f159e3924b3d7238
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-05-09 00:43:56 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,10 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.float :price
+    end
+  end
+end
